561ed944ff71c736315b0102fe8098e3d7a699eb
Neel Thomas <neeltom92@gmail.com>
2022-08-31 01:53:56 +0530

    Fix: update the aws region in vars.tf
    Fix: update the aws region in vars.tf

--- eks/vars.tf
+++ eks/vars.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "EKS cluster creation region"
+  type        = string
+}
+
+
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
